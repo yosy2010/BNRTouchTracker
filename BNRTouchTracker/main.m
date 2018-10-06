83a1e47c3d0e72bdab7d2909b1ed9bca095b84b5
Yasser Altamimi <yosy2010@gmail.com>
2018-10-06 10:51:49 +0300

    Initial Commit

---
+++ BNRTouchTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BNRTouchTracker
+//
+//  Created by YASSER ALTAMIMI on 06/10/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
